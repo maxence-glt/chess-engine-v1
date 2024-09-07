ed3596173d3a8fc563a1a76a08e5d0741df2a20c
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-09-07 00:38:14 -0400

    GTest linking finished

--- tests/bitboard_test.cpp
+++ tests/bitboard_test.cpp
@@ -2,8 +2,11 @@
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
-// Expect two strings not to be equal.
-EXPECT_STRNE("hello", "world");
-// Expect equality.
-EXPECT_EQ(7 * 6, 42);
+    ASSERT_EQ(3, 4) << "test1";
+    ASSERT_EQ(3, 3);
+}
+
+TEST(HelloTest, OtherBasicAssertions) {
+    ASSERT_EQ(4, 4) << "test2";
+    ASSERT_EQ(3, 3);
 }
